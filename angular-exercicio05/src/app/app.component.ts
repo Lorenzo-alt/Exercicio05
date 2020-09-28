33fa20be0661866ebf869d6b3aa9caef738847ef
Lorenzo <lorenzo.juliati@terra.com.br>
2020-09-28 14:48:42 -0300

    Adicionado documentos +mais e bootstrap

--- angular-exercicio05/src/app/app.component.ts
+++ angular-exercicio05/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-exercicio04';
+  title = 'angular-exercicio05';
+  imagemLP = '/assets/imagens/linguagem_de_programacao.jpg'
+  imagemDesigner = '/assets/imagens/web_design.png'
+  imagemGame = '/assets/imagens/desenvolvimento_game.jpg'
+  imagemBanco = '/assets/imagens/banco_de_dados.jpg'
+  imagemCG = '/assets/imagens/computacao_grafica.jpg'
+  logo = 'assets/imagens/logo.png'
 }
